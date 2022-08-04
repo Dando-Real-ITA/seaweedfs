a9f7bca4b60990f7f7cf86c9a01bc45308668e17
Gaspare Iengo <gaspareiengo@gmail.com>
2022-08-04 14:39:02 +0000

    Added hosts.sh
    Allows add or remove hostnames in /etc/hosts

---
+++ docker/hosts.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+# 2022-08-04 14:11:08
+
+########################################################################################################################################################################################################################
+
+# PATH TO YOUR HOSTS FILE
+ETC_HOSTS=/etc/hosts
+
+########################################################################################################################################################################################################################
+
+yell() { echo "$0: $*" >&2; }
+die() { yell "$*"; exit 111; }
+try() { "$@" || die "cannot $*"; }
+
+remove() {
+  if [ -z "$1" ]; then
+    die "Exiting... invalid arguments";
+  fi
+
+  # Hostname to add/remove.
+  HOSTNAME=$1
+
+  # TESTARE CON GREP REGEX
+  if [[ -n "$(grep -E "\s+$HOSTNAME$" /etc/hosts)" ]]; then
+    echo "$HOSTNAME Found in $ETC_HOSTS, Removing now...";
+    try sed -i -r "/\s+$HOSTNAME/d" "$ETC_HOSTS";
+  else
+    yell "$HOSTNAME was not found in your $ETC_HOSTS";
+  fi
+}
+
+add() {
+  if [ -z "$1" ] || [ -z "$2" ]; then
+    die "Exiting... invalid arguments";
+  fi
+
+  # IP to add/remove.
+  IP=$1
+  # Hostname to add/remove.
+  HOSTNAME=$2
+
+  host_line="$IP $HOSTNAME"
+
+  if [[ -n "$(grep -E "\s+$HOSTNAME$" /etc/hosts)" ]]; then
+    echo "$HOSTNAME Found in $ETC_HOSTS, Replacing now...";
+    try sed -i -r "s/^.*\s+$HOSTNAME.*$/$host_line/" "$ETC_HOSTS";
+  else
+    echo "Adding $HOSTNAME to $ETC_HOSTS...";
+    echo "$host_line" >> /etc/hosts
+  fi
+
+  added_line=$(grep -E "\s+$HOSTNAME$" /etc/hosts)
+  if [[ -n "$added_line" ]]; then
+    echo "$HOSTNAME was added succesfully: '$added_line'";
+  else
+    die "Failed to add $HOSTNAME";
+  fi
+}
+
+########################################################################################################################################################################################################################
+
+if [ -z "$1" ] || [ -z "$2" ]; then
+  die "Exiting... invalid arguments";
+fi
+
+$@
+
+########################################################################################################################################################################################################################
