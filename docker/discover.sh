eb2a3b92c19d600f6dc24df27832e311e852a2df
Gaspare Iengo <gaspareiengo@gmail.com>
2022-08-04 17:04:41 +0000

    Added discover.sh
    It will be called by tcpserver and tcpclient
    Client and Server send each other their ip and hostname and update the /etc/hosts file

---
+++ docker/discover.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+# 2022-08-04 15:46:24
+
+########################################################################################################################################################################################################################
+
+# Check if file descriptor 6 is open
+if [[ -n "$(ls /proc/$$/fd/ | grep 6)" ]]; then
+  # Client
+
+  # Send exported local ip and hostname
+  echo "$LOCAL_IP $LOCAL_HOSTNAME" >&7
+
+  # Receive remote ip and hostname
+  read -u 6 REMOTE_IP REMOTE_HOSTNAME
+
+  # Add/Update host file
+  /hosts.sh add $REMOTE_IP $REMOTE_HOSTNAME >&2
+else
+  # Server
+
+  # Receive remote ip and hostname
+  read -u 0 REMOTE_IP REMOTE_HOSTNAME
+
+  # Add/Update host file
+  /hosts.sh add $REMOTE_IP $REMOTE_HOSTNAME >&2
+
+  # Send exported local ip and hostname
+  echo "$LOCAL_IP $LOCAL_HOSTNAME" >&1
+fi
+
+########################################################################################################################################################################################################################
