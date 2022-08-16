eef2c33fd6c859a0709925dc715422428d7d9422
Gaspare Iengo <gaspareiengo@gmail.com>
2022-08-16 11:31:00 +0000

    Updated discover to optionally add host

--- docker/discover.sh
+++ docker/discover.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
-# 2022-08-04 17:08:53
+# 2022-08-13 10:37:41
 
 ########################################################################################################################################################################################################################
 
@@ -14,8 +14,10 @@ if [[ -n "$(ls /proc/$$/fd/ | grep 6)" ]]; then
   # Receive remote ip and hostname
   read -u 6 REMOTE_IP REMOTE_HOSTNAME
 
-  # Add/Update host file
-  /hosts.sh add $REMOTE_IP $REMOTE_HOSTNAME >&2
+  if [[ ${ADD_HOST:-true} != "false" ]]; then
+    # Add/Update host file
+    /hosts.sh add $REMOTE_IP $REMOTE_HOSTNAME >&2
+  fi
 
   # Return remote hostname to calling script
   echo $REMOTE_HOSTNAME
@@ -25,8 +27,10 @@ else
   # Receive remote ip and hostname
   read -u 0 REMOTE_IP REMOTE_HOSTNAME
 
-  # Add/Update host file
-  /hosts.sh add $REMOTE_IP $REMOTE_HOSTNAME >&2
+  if [[ ${ADD_HOST:-true} != "false" ]]; then
+    # Add/Update host file
+    /hosts.sh add $REMOTE_IP $REMOTE_HOSTNAME >&2
+  fi
 
   # Send exported local ip and hostname
   echo "$LOCAL_IP $LOCAL_HOSTNAME" >&1
