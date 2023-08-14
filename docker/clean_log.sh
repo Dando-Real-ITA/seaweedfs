72a128bee3ac7d2d92add700b6b98d6116a1ca69
Gaspare Iengo <gaspareiengo@gmail.com>
2023-08-14 09:31:22 +0000

    Updated clean log to ensure the same filer is used for delete

--- docker/clean_log.sh
+++ docker/clean_log.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
-# 2023-08-12 20:20:41
+# 2023-08-14 09:25:50
 
 ########################################################################################################################################################################################################################
 
@@ -10,11 +10,11 @@ WEED_KEEP_DAYS=${WEED_KEEP_DAYS:=2}
 
 echo "fs.du /topics/.system/log;" | /usr/bin/weed shell -master=${WEED_MASTER}
 
-for row in $(curl -s -H "Accept: application/json" "http://${WEED_FILER}/topics/.system/log?pretty=y&lastFileName=&limit=1000" | jq '.Entries[] | select(now - (.Crtime | fromdate) > (86400 * $keep_days)) | { "path" : .FullPath } | @base64' -c -M -r --argjson keep_days ${WEED_KEEP_DAYS}); do
+for row in $(curl -s -H "Accept: application/json" "http://${WEED_FILER}/topics/.system/log?lastFileName=&limit=1000" | jq '.Entries[] | select(now - (.Crtime | fromdate) > (86400 * $keep_days)) | { "path" : .FullPath } | @base64' -c -M -r --argjson keep_days ${WEED_KEEP_DAYS}); do
   _jq() {
     echo ${row} | base64 -d | jq -r ${1}
   }
-  echo "fs.rm -rf $(_jq '.path')" | /usr/bin/weed shell -master=${WEED_MASTER}
+  curl -X DELETE "http://${WEED_FILER}$(_jq '.path')?recursive=true&ignoreRecursiveError=true"
 done
 
 echo "fs.du /topics/.system/log;" | /usr/bin/weed shell -master=${WEED_MASTER}
