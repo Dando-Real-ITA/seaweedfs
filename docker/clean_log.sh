e96a81627348139e2d4663ac3a2fe5358e7f309b
Gaspare Iengo <gaspareiengo@gmail.com>
2024-03-30 05:56:03 +0000

    Updated clean_log to use new purge command

--- docker/clean_log.sh
+++ docker/clean_log.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
-# 2023-12-07 15:05:19
+# 2024-03-30 05:34:39
 
 ########################################################################################################################################################################################################################
 
@@ -13,12 +13,14 @@ echo "Executing cleanup for ${WEED_FILER} with ${WEED_KEEP_DAYS} keep days"
 
 echo "fs.du /topics/.system/log;" | timeout -s SIGKILL 1m /usr/bin/weed shell -master=${WEED_MASTER} -filer=${WEED_FILER}
 
-for row in $(curl -s -H "Accept: application/json" "http://${WEED_FILER}/topics/.system/log?lastFileName=&limit=1000" | jq '.Entries[] | select(now - (.Crtime | fromdate) > (86400 * $keep_days)) | { "path" : .FullPath } | @base64' -c -M -r --argjson keep_days ${WEED_KEEP_DAYS}); do
-  _jq() {
-    echo ${row} | base64 -d | jq -r ${1}
-  }
-  timeout -s SIGKILL 1h curl -s -X DELETE "http://${WEED_FILER}$(_jq '.path')?recursive=true&ignoreRecursiveError=true"
-done
+# for row in $(curl -s -H "Accept: application/json" "http://${WEED_FILER}/topics/.system/log?lastFileName=&limit=1000" | jq '.Entries[] | select(now - (.Crtime | fromdate) > (86400 * $keep_days)) | { "path" : .FullPath } | @base64' -c -M -r --argjson keep_days ${WEED_KEEP_DAYS}); do
+#   _jq() {
+#     echo ${row} | base64 -d | jq -r ${1}
+#   }
+#   timeout -s SIGKILL 1h curl -s -X DELETE "http://${WEED_FILER}$(_jq '.path')?recursive=true&ignoreRecursiveError=true"
+# done
+
+echo "fs.log.purge -v -daysAgo ${WEED_KEEP_DAYS};" | timeout -s SIGKILL 120m /usr/bin/weed shell -master=${WEED_MASTER} -filer=${WEED_FILER}
 
 echo "fs.du /topics/.system/log;" | timeout -s SIGKILL 1m /usr/bin/weed shell -master=${WEED_MASTER} -filer=${WEED_FILER}
 echo "***"
